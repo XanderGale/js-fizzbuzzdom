2beb48b8766dc5c4ea8dbf1e8cd49e48b9f25409
XanderGale <alessandro.galeazzi@icloud.com>
2021-11-17 04:26:14 +0100

    fixed i start from 1

--- milestone1/js/script.js
+++ milestone1/js/script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 console.log(container);
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     console.log(i);
 
     const boxNumber = `<div class="box number">${i}</div>`;
